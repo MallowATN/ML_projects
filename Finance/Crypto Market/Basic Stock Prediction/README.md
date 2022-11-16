6f65269308904cf0f277f11f17490e1d0ffd9436
MallowATN <anthonynguyenatn@gmail.com>
2022-11-16 09:45:10 -0600

    update

--- Finance/Crypto Market/Basic Stock Prediction/README.md
+++ Finance/Crypto Market/Basic Stock Prediction/README.md
@@ -1,4 +1,15 @@
 <h1> ETH Basic Prediction Bot </h1>
-<h4> The ETH dataset was webscraped from CoinMarketCap. The K-nearest neighbors algorithm was used for testing purpose only. </h4>
+<div class = "Summary">
+    <h4> The main goal of this project is to show the basic element of prediction using ETH dataset webscraped from CoinMarketCap. The idea behind this project is to find some useful features from professional traders and apply it using Machine Learning. </h4>
+    <br>
+    <h4> Below is the result of selectKbest to find which features are highly correlated with the closing column (Target Variable).
+    <img src="images/SelectKBest.png" alt="featureselect">
+    <br><br>
+    <h2>I used a Random Forest Regressor algorithm for the basic ETH prediction and evaluated the model with R2 scoring.</h2>
+    <img src="images/SelectKBest.png" alt="featureselect">
+    <br>
+    <h4> Note that this does not cover the whole features that needs to be feature engineered, I only applied the basic principles of Machine Learning for my own knowledge. If you want to learn more about feature implimentation, you can search on investopedia and create the custom features, much like how I've set some of them up in one of my notebooks.
+</div>
+
 
 <h6> None of this is considered financial advice, I will not be responsible for any transactions involving crypto or stock market. This is for educational purposes only.</h6>
--- Finance/Crypto Market/Basic Stock Prediction/train.py
+++ Finance/Crypto Market/Basic Stock Prediction/train.py
@@ -2,10 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from functions import *
-
-
 
 
 #########################################
@@ -13,25 +12,18 @@ from functions import *
 #########################################
 
 # Load the data
-df = pd.read_csv("ETH_hist.csv")
+df = pd.read_csv("ETH-USD.csv")
 
 # Prepare the dataframe
-for column in df:
-    df.columns = ['Date', 'Open','High','Low','Close','Volume','Market Cap']
-for i in df.columns[1:7]:
-    df[i] = df[i].str.replace(',','')
-    df[i] = df[i].str.replace('$','', regex=True)
-    df[i] = df[i].astype(float)
-df['Volume'] = df['Volume'].astype(np.int64)
-df['Market Cap'] = df['Market Cap'].astype(np.int64)
-
 df = df.set_index('Date')
 
 SMA(df)
 EMA(df)
 RSIs(df)
 
-df=df.drop(['pos','neg','Log Return','diff','Open','High','Low'], axis=1)
+df=df.drop(['pos','neg','diff','Open','High','Low'], axis=1)
+#Drop the nan rows bc we had to use rolling method to obtain the MA & RSI
+df = df.dropna(axis=0)
 
 # Split data into train/test split
 X = df.drop('Close',axis=1)
@@ -47,8 +39,8 @@ y_train, y_validation = Y[0:train_size], Y[train_size:len(X)]
 #########################################
 
 # Fit model on train
-model = KNeighborsRegressor(n_neighbors=3)
-model.fit(X_train, y_validation)
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
 
 #Report training set score
 train_score = model.score(X_train, y_train)*100
@@ -59,29 +51,3 @@ test_score = model.score(X_validation, y_validation)*100
 with open("metrics.txt", 'w') as outfile:
     outfile.write("Training variance explained: %2.1f%%\n"%train_score)
     outfile.write("Test variance explained: %2.1f%%\n"%test_score)
-
-#########################################
-######## PLOT FEATURE IMPORTANCE ########
-#########################################
-
-importances = model.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False)
-
-#image formatting
-axis_fs = 18
-title_fs = 22
-sns.set(style="whitegrid")
-
-ax = sns.barplot(x="importance", y="feature", data = feature_df)
-ax.set_xlabel('Importance"', fontsize=axis_fs)
-ax.set_ylabel('Feature', fontsize=axis_fs)
-ax.set_title('Residuals', fontsize=title_fs)
-
-#square aspect ratio
-ax.plot([1,10], [1,10], 'black', linewidth=1)
-plt.ylim((2.5, 8.5))
-plt.xlim((2.5, 8.5))
-plt.tight_layout()
-plt.savefig("images/residuals.png", dpi=120)
